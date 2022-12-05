d533ae288b07bc821654a142ea641e311624ac7a
Ryan Young <ryanbyoung1@gmail.com>
2022-12-05 18:08:47 -0500

    added form section for initials and local storage

--- assets/script.js
+++ assets/script.js
@@ -9,9 +9,12 @@ var penalty = 10;
 var score = 0;
 var questionContainer = document.querySelector(".questionWrapper");
 var endGame = document.querySelector(".endGame");
+var highScoreList = document.querySelector(".highscore");
 
 questionContainer.style.display = "none";
 endGame.style.display = "none";
+highScoreList.style.display = "none";
+
 
 
 var timerMessage = function () {
@@ -22,9 +25,8 @@ var timerMessage = function () {
     timeEl.textContent = timeLeft + " " + label;
 };
 
-// Make this a function for highscore tracking
 var displayEndGame = function() {
-    timeEl.textContent = "You've run out of time!";
+    timeEl.textContent = "End of Quiz";
     timeEl.style.color = "red";
     questionContainer.style.display = "none";
     endGame.style.display = "block";
@@ -64,7 +66,6 @@ var advance = function (event) {
     if (element.matches(".question button")) {
         if (element.dataset.answer === correctAnswers[click]) {
             score = score + 10;
-            console.log(score);
         } else {
             timeLeft = timeLeft - penalty;
         }
@@ -86,4 +87,20 @@ start.addEventListener("click", function () {
     questionContainer.style.display = "flex";
     quiz();
     setTime();
+});
+
+// High Score Function
+
+var submitButton = document.querySelector("#submit");
+    
+submitButton.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    var initials = document.querySelector("#initials").value;
+
+    localStorage.setItem("initials", initials);
+    localStorage.setItem("score", score);
+
+    endGame.style.display = "none";
+    highScoreList.style.display = "block";
 });
