d6f85d3e167935a4f07368233bbce3b4fddbc637
Ryan Young <ryanbyoung1@gmail.com>
2022-12-04 22:17:31 -0500

    added timer and message functions

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,7 @@ var container = document.querySelectorAll("section");
 var questions = document.querySelectorAll(".question");
 var correctAnswers = ["0", "2", "1", "1", "2"];
 var start = document.querySelector(".start");
+var timeEl = document.querySelector(".timer");
 
 // var displayQuestion = function () {
 //     for (var question of questions) {
@@ -54,7 +55,37 @@ displayQuestion();
 
 };
 
+var timeLeft = 60;
+
+var timerMessage = function () {
+    var label = "seconds left";
+    if (timeLeft === 1) {
+        label = "second left";
+    }
+    timeEl.textContent = timeLeft + " " + label;
+};
+
+var sendMessage = function() {
+    timeEl.textContent = "Oh no! You've run out of time!";
+    timeEl.style.color = "red";
+};
+
+var setTime = function () {
+    timerMessage();
+    var timerInterval = setInterval(function () {
+        timeLeft--;
+    timerMessage();
+
+    if(timeLeft === 0) {
+        clearInterval(timerInterval);
+        sendMessage();
+    }
+
+    }, 1000);
+};
+
 start.addEventListener("click", function () {
     start.style.display = "none";
     quiz();
+    setTime();
 });
