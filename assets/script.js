181080b34d6238def66fc0567d0e5fd748c38a4e
Ryan Young <ryanbyoung1@gmail.com>
2022-12-04 19:25:43 -0500

    added data index to question sections and started display function

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,16 @@
 var forwardEl = document.querySelector("#forward");
 var click = 0;
 var questions = document.querySelectorAll(".question");
+var start = document.querySelector(".start");
+
+var displayQuestion = function () {
+    
+};
+
+
+
+// var startQuiz = function() {
+  
+// };
+
+// start.addEventListener("click", startQuiz);
