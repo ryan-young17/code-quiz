a2800567c72ffb5301301d081dce0d6e774c1003
Ryan Young <ryanbyoung1@gmail.com>
2022-12-04 23:35:23 -0500

    added in score variable

--- assets/script.js
+++ assets/script.js
@@ -6,50 +6,8 @@ var start = document.querySelector(".start");
 var timeEl = document.querySelector(".timer");
 var timeLeft = 60;
 var penalty = 10;
+var score = 0;
 
-// Quiz Function
-
-var quiz = function () {
-
-// var displayQuestion = function () {
-//     for (var section of container) {
-//         if (section.dataset.index != click) {
-//             section.style.display = "none";
-//         } else {
-//             section.style.display = "block";    
-//         }
-//     }
-// }
-
-var displayQuestion = function () {
-    for (var question of questions) {
-        if (question.dataset.index != click) {
-            question.style.display = "none";
-        } else {
-            question.style.display = "block";
-        }
-    }
-};
-
-var advance = function (event) {
-    var element = event.target;
-    if (element.matches(".question button")) {
-        // var choice = element.dataset.answer === correctAnswers[click];
-        if (element.dataset.answer !== correctAnswers[click]) {
-            timeLeft = timeLeft - penalty;
-        }
-        if (click < questions.length - 1) {
-            click++;
-        }
-        displayQuestion();
-    }
-};
-
-document.addEventListener("click", advance);
-
-displayQuestion();
-
-};
 
 // Timer Functions
 
@@ -79,7 +37,55 @@ var setTime = function () {
 
     }, 1000);
 
+};
 
+// Quiz Function
+
+var quiz = function () {
+
+// var displayQuestion = function () {
+//     for (var section of container) {
+//         if (section.dataset.index != click) {
+//             section.style.display = "none";
+//         } else {
+//             section.style.display = "block";    
+//         }
+//     }
+// }
+
+var displayQuestion = function () {
+    for (var question of questions) {
+        if (question.dataset.index != click) {
+            question.style.display = "none";
+        } else {
+            question.style.display = "block";
+        }
+    }
+};
+
+var advance = function (event) {
+    var element = event.target;
+    if (element.matches(".question button")) {
+        // var choice = element.dataset.answer === correctAnswers[click];
+        // if (element.dataset.answer !== correctAnswers[click]) {
+        //     timeLeft = timeLeft - penalty;
+        // }
+        if (element.dataset.answer === correctAnswers[click]) {
+            score = score + 10;
+            console.log(score);
+        } else {
+            timeLeft = timeLeft - penalty;
+        }
+        if (click < questions.length - 1) {
+            click++;
+        }
+        displayQuestion();
+    }
+};
+
+document.addEventListener("click", advance);
+
+displayQuestion();
 
 };
 
