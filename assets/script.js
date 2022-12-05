b983112701c789cbbabc214d611075e305157fb0
Ryan Young <ryanbyoung1@gmail.com>
2022-12-04 21:51:15 -0500

    wrapped question functions in a function and added eventListener to start section

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@ var click = 0;
 var container = document.querySelectorAll("section");
 var questions = document.querySelectorAll(".question");
 var correctAnswers = ["0", "2", "1", "1", "2"];
-var start = document.querySelector(".start button");
+var start = document.querySelector(".start");
 
 // var displayQuestion = function () {
 //     for (var question of questions) {
@@ -14,20 +14,33 @@ var start = document.querySelector(".start button");
 //     }
 // };
 
+var quiz = function () {
+
+// var displayQuestion = function () {
+//     for (var section of container) {
+//         if (section.dataset.index != click) {
+//             section.style.display = "none";
+//         } else {
+//             section.style.display = "block";    
+//         }
+//     }
+// }
+
 var displayQuestion = function () {
-    for (var section of container) {
-        if (section.dataset.index != click) {
-            section.style.display = "none";
+    for (var question of questions) {
+        if (question.dataset.index != click) {
+            question.style.display = "none";
         } else {
-            section.style.display = "block";    
+            question.style.display = "block";
         }
     }
-}
+};
 
 var advance = function (event) {
     var element = event.target;
-    if (element.matches("button")) {
+    if (element.matches(".question button")) {
         var choice = element.dataset.answer === correctAnswers[click];
+        // 
         if (click < questions.length - 1) {
             click++;
         }
@@ -38,3 +51,10 @@ var advance = function (event) {
 document.addEventListener("click", advance);
 
 displayQuestion();
+
+};
+
+start.addEventListener("click", function () {
+    start.style.display = "none";
+    quiz();
+});
